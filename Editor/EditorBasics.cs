b07f1e8d4e3d6245aa9079ffb0fffbd21e9334f3
zehreken <zehreken@gmail.com>
2021-05-23 23:29:40 +0200

    Add start scene picker

--- Editor/EditorBasics.cs
+++ Editor/EditorBasics.cs
@@ -2,11 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEditor.SceneManagement;
 
 namespace Mogze.Core
 {
-    public class EditorBasics
+    public class EditorBasics : EditorWindow
     {
+        void OnGUI()
+        {
+            EditorSceneManager.playModeStartScene = (SceneAsset)EditorGUILayout.ObjectField(new GUIContent("Start Scene"), EditorSceneManager.playModeStartScene, typeof(SceneAsset), false);
+        }
+
+        [MenuItem("Mogze/Start Scene Picker")]
+        static void Open()
+        {
+            GetWindow<EditorBasics>();
+        }
+
         [MenuItem("Mogze/Clear All")]
         static void ClearPlayerPrefs()
         {
