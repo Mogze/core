cb633cb40bad3cb718b6110b4efd7b215ff42b2f
zehreken <zehreken@gmail.com>
2021-07-20 20:37:17 +0200

    Add StringUtilities

---
+++ Runtime/StringUtilities/StringExtensions.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Text;
+
+namespace Mogze.Core.StringUtilities
+{
+	public static class StringExtensions
+	{
+		public static string Italic(this string s)
+		{
+			return $"<i>{s}</i>";
+		}
+
+		public static string Bold(this string s)
+		{
+			return $"<b>{s}</b>";
+		}
+
+		public static string Color(this string s, Color color)
+		{
+            var hexColor = ColorUtility.ToHtmlStringRGBA(color);
+			return $"<color=#{hexColor}>{s}</color>";
+		}
+
+		public static string ToPrintable<T>(this T obj)
+		{
+            var sb = new StringBuilder("");
+			var fields = typeof(T).GetFields();
+			foreach (var fieldInfo in fields)
+			{
+                sb.Append($"{fieldInfo.Name}: {fieldInfo.GetValue(obj)}\n");
+			}
+
+			return sb.ToString();
+		}
+	}
+}
