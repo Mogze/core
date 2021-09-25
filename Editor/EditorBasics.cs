f8d693fdac46c1983aa065c7b6ef0293cbccfee6
zehreken <zehreken@gmail.com>
2021-09-25 16:05:41 +0200

    Improve start scene picker

--- Editor/EditorBasics.cs
+++ Editor/EditorBasics.cs
@@ -6,19 +6,50 @@ using UnityEditor.SceneManagement;
 
 namespace Mogze.Core
 {
+    [InitializeOnLoad]
+    public class Startup
+    {
+        static Startup()
+        {
+            if (PlayerPrefs.HasKey(EditorBasics.StartScenePathKey))
+            {
+                var sceneAsset = AssetDatabase.LoadAssetAtPath<SceneAsset>(PlayerPrefs.GetString(EditorBasics.StartScenePathKey));
+                EditorSceneManager.playModeStartScene = sceneAsset;
+            }
+        }
+    }
     public class EditorBasics : EditorWindow
     {
-        void OnGUI()
-        {
-            EditorSceneManager.playModeStartScene = (SceneAsset)EditorGUILayout.ObjectField(new GUIContent("Start Scene"), EditorSceneManager.playModeStartScene, typeof(SceneAsset), false);
-        }
+        public const string StartScenePathKey = "StartScenePathKey";
 
         [MenuItem("Mogze/Start Scene Picker")]
-        static void Open()
+        static void Init()
         {
-            GetWindow<EditorBasics>();
+            GetWindow<EditorBasics>().Show();
+            if (PlayerPrefs.HasKey(StartScenePathKey))
+            {
+                var sceneAsset = AssetDatabase.LoadAssetAtPath<SceneAsset>(PlayerPrefs.GetString(StartScenePathKey));
+                EditorSceneManager.playModeStartScene = sceneAsset;
+            }
         }
 
+        void OnGUI()
+        {
+            var sceneAsset = (SceneAsset)EditorGUILayout.ObjectField(new GUIContent("Start Scene"), EditorSceneManager.playModeStartScene, typeof(SceneAsset), false);
+            EditorSceneManager.playModeStartScene = sceneAsset;
+            var startScenePath = AssetDatabase.GetAssetPath(sceneAsset);
+
+            if (GUILayout.Button("Save"))
+            {
+                if (!string.IsNullOrEmpty(startScenePath))
+                {
+                    PlayerPrefs.SetString(StartScenePathKey, startScenePath);
+                    PlayerPrefs.Save();
+                }
+            }
+        }
+
+        // This should be somewhere else
         [MenuItem("Mogze/Clear All")]
         static void ClearPlayerPrefs()
         {
