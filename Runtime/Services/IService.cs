71ac795d22892104a593abd53e1bc735d6f9767d
zehreken <zehreken@gmail.com>
2022-05-01 23:42:05 +0200

    [#1] Add Pause method to IService

--- Runtime/Services/IService.cs
+++ Runtime/Services/IService.cs
@@ -5,6 +5,10 @@ namespace Mogze.Core.Services
     public interface IService
     {
         Task Initialize();
+        /// <summary>
+        /// To be paired with OnApplicationPause event in Unity.
+        /// </summary>
+        void Pause(bool isPaused);
         void Close();
     }
 }
